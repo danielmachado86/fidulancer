7d77d7f359e18eadbb1a190cc0b92a47ed079142
Daniel Machado <danielmcis@gmail.com>
2023-12-24 06:02:16 +0000

    Add validation middleware to user signup route

--- api/src/routes/users.ts
+++ api/src/routes/users.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import * as UserController from "../controllers/users";
 import { requiresAuth } from "../middleware/auth";
+import validate from "../middleware/validation";
+import { User } from "../models/user";
 
 const router = express.Router();
 
 router.get("/", requiresAuth, UserController.getAuthenticatedUser);
 
-router.post("/signup", UserController.signUp);
+router.post("/signup", validate(User), UserController.signUp);
 
 router.post("/login", UserController.login);
 
