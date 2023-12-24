d8af6e4f1ad5c8847f8de0811545fc6e83fa503d
Daniel Machado <danielmcis@gmail.com>
2023-12-24 04:12:22 +0000

    Add validation middleware

---
+++ api/src/middleware/validation.ts
@@ -0,0 +1,22 @@
+import { NextFunction, Request, Response } from "express";
+import { AnyZodObject, ZodError } from "zod";
+
+const validate = (schema: AnyZodObject) => {
+    return async (req: Request, res: Response, next: NextFunction) => {
+        try {
+            await schema.parseAsync(req.body);
+            next();
+        } catch (error) {
+            if (error instanceof ZodError) {
+                res.status(400).json({
+                    message: "Validation error",
+                    errors: error.errors,
+                });
+            } else {
+                next(error);
+            }
+        }
+    };
+};
+
+export default validate;
