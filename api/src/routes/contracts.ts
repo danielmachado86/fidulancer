b809664d9da6eb1a64902271615f428bd39023a8
Daniel Machado <danielmcis@gmail.com>
2023-12-28 15:41:21 +0000

    Add validation middleware to createContract route

--- api/src/routes/contracts.ts
+++ api/src/routes/contracts.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import * as ContractController from "../controllers/contracts";
 import { requiresAuth } from "../middleware/auth";
+import validate from "../middleware/validation";
+import { ContractBaseDocument } from "../models/contract";
 
 const router = express.Router();
 
 router.get("/", requiresAuth, ContractController.getContracts);
 router.get("/:contractId", requiresAuth, ContractController.getContract);
-router.post("/", requiresAuth, ContractController.createContract);
+router.post(
+    "/",
+    validate(ContractBaseDocument),
+    requiresAuth,
+    ContractController.createContract
+);
 router.patch("/:contractId", requiresAuth, ContractController.updateContract);
 router.delete("/:contractId", requiresAuth, ContractController.deleteContract);
 
