7f32d3cf5eef7c309221e67e4fdc9d96155abb50
Daniel Machado <danielmcis@gmail.com>
2023-12-28 15:41:14 +0000

    Refactor user model and interfaces

--- api/src/models/user.ts
+++ api/src/models/user.ts
@@ -1,4 +1,4 @@
-import { WithId } from "mongodb";
+import { ObjectId } from "mongodb";
 import { z } from "zod";
 import { db } from "../db";
 
@@ -7,17 +7,19 @@ export const LoginSchema = z.object({
     password: z.string().min(8),
 });
 
-export const User = LoginSchema.extend({
+export const UserInterface = LoginSchema.extend({
     name: z.string().min(1),
 });
 
-export const UserDocument = User.extend({
+export const UserBaseDocument = UserInterface.extend({
+    _id: z.instanceof(ObjectId).default(() => new ObjectId()),
     createdAt: z.date().default(() => new Date()),
     updatedAt: z.date().default(() => new Date()),
 });
 
-export type User = z.infer<typeof User>;
+// create a type for a user without the password
+
+export type UserInterface = z.infer<typeof UserInterface>;
 export type LoginSchema = z.infer<typeof LoginSchema>;
-export type UserDocument = z.infer<typeof UserDocument>;
-export type UserWithId = WithId<UserDocument>;
-export const Users = db.collection<UserDocument>("users");
+export type UserBaseDocument = z.infer<typeof UserBaseDocument>;
+export const Users = db.collection<UserBaseDocument>("users");
