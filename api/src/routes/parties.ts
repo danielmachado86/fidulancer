a308e3d50c84c43824a8d629ef7f010290291b10
Daniel Machado <danielmcis@gmail.com>
2023-12-28 15:41:27 +0000

    Add validation middleware to createParty route

--- api/src/routes/parties.ts
+++ api/src/routes/parties.ts
@@ -1,12 +1,19 @@
 import express from "express";
 import * as PartyController from "../controllers/parties";
 import { requiresAuth } from "../middleware/auth";
+import validate from "../middleware/validation";
+import { PartyBaseDocument } from "../models/party";
 
 const router = express.Router();
 
 router.get("/", requiresAuth, PartyController.getParties);
 router.get("/:partyId", requiresAuth, PartyController.getParty);
-router.post("/", requiresAuth, PartyController.createParty);
+router.post(
+    "/",
+    requiresAuth,
+    validate(PartyBaseDocument),
+    PartyController.createParty
+);
 // router.patch("/:partyd", requiresAuth, PartyController.updateContract);
 // router.delete("/:partyd", requiresAuth, PartyController.deleteContract);
 
